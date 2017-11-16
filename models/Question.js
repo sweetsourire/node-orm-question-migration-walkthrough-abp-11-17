c91b18079e3fdd3fc2ff405769a233b5cd09b7d6
Bomi <bkim1289@gmail.com>
2017-11-16 20:50:41 +0000

    Done.

--- models/Question.js
+++ models/Question.js
@@ -3,7 +3,17 @@
 const db = require("../config/db")
 
 class Question{
-
+  static CreateTable() {
+    return new Promise(function(resolve){
+      const sql = `CREATE TABLE questions (
+        id INTEGER PRIMARY KEY,
+        content TEXT
+      )`
+      db.run(sql, function(){
+      resolve("quetions table created")
+      })
+    })
+  }
 }
 
 
